1cf8d7f1582c84feb9580484aa5e011eef623d47
PabloPicas0 <pawel.fe@gmail.com>
2023-02-15 17:24:08 +0100

    Added legend

--- script.js
+++ script.js
@@ -9,7 +9,57 @@ const w = 1600;
 const h = 600;
 const padding = 60;
 
+const colors = [
+  "#a50026",
+  "#d73027",
+  "#f46d43",
+  "#fdae61",
+  "#fee090",
+  "#ffffbf",
+  "#e0f3f8",
+  "#abd9e9",
+  "#74add1",
+  "#4575b4",
+  "#313695",
+].reverse();
+
 const svg = d3.select("section").append("svg").attr("width", w).attr("height", h);
+
+const legend = svg
+  .append("g")
+  .attr("transform", `translate(600, ${h - 20})`)
+  .attr("id", "legend");
+
+const convertMonths = (monthNumber) => {
+  switch (monthNumber) {
+    case 1:
+      return "January";
+    case 2:
+      return "Febuary";
+    case 3:
+      return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+    case 6:
+      return "June";
+    case 7:
+      return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+    default:
+      return "";
+  }
+};
 
 const render = (data) => {
   //Base temperature
@@ -26,8 +76,8 @@ const render = (data) => {
   const yMax = d3.max(data.monthlyVariance, (data) => data.month);
 
   ////Lowest and highest temperatures for legendScale
-  const lMin = d3.min(belowAvg, (data) => data.variance);
-  const lMax = d3.max(aboveAvg, (data) => data.variance);
+  const lMin = d3.min(belowAvg, (data) => baseTemp + data.variance);
+  const lMax = d3.max(aboveAvg, (data) => baseTemp + data.variance);
 
   //x, y and legend Scale
   const xScale = d3
@@ -38,48 +88,41 @@ const render = (data) => {
     .scaleLinear()
     .domain([yMax + 0.5, yMin - 0.5]) //We are adding some space to top and bottom of yAxis by adding this 0,5
     .range([h - padding, padding]);
+
+  //Threshold for legend ticks
+  const treshold = d3
+    .scaleThreshold()
+    //Source for domain: FCC Project -> DevTools -> Sources -> script.js
+    .domain(
+      ((min, max, numColors) => {
+        const array = [];
+        const range = (max - min) / numColors;
+        const base = min;
+        for (let i = 1; i < numColors; i++) {
+          array.push(base + i * range);
+        }
+        return array;
+      })(lMin, lMax, colors.length)
+    )
+    .range(colors);
+
   const lScale = d3
     .scaleLinear()
     .domain([lMin, lMax])
     .range([padding, w / 4]);
 
-  //x, y nad legend Axis
+  //x, y and legend Axis
   const xAxis = d3
     .axisBottom(xScale)
     .tickFormat((x) => Math.floor(x))
     .ticks(20);
   //Doing axis like this was pure experiment but it works so its okay
-  const yAxis = d3.axisLeft(yScale).tickFormat((x) => {
-    switch (x) {
-      case 1:
-        return "January";
-      case 2:
-        return "Febuary";
-      case 3:
-        return "March";
-      case 4:
-        return "April";
-      case 5:
-        return "May";
-      case 6:
-        return "June";
-      case 7:
-        return "July";
-      case 8:
-        return "August";
-      case 9:
-        return "September";
-      case 10:
-        return "October";
-      case 11:
-        return "November";
-      case 12:
-        return "December";
-      default:
-        return "";
-    }
-  });
-  const lAxis = d3.axisBottom(lScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat((x) => convertMonths(x));
+  const lAxis = d3
+    .axisBottom(lScale)
+    .tickValues(treshold.domain())
+    .tickFormat(d3.format(".1f")) //creates axis for legend, change their ticks values and format them to fixed precision;
+    .tickSize(-30);
 
   const xAxisG = svg
     .append("g")
@@ -95,11 +138,34 @@ const render = (data) => {
     .call(yAxis)
     .call((g) => g.select(".domain").remove());
 
-  const legend = svg
+  const legendAxis = legend.append("g").call(lAxis);
+
+  legendAxis.select(".domain").remove();
+
+  //Source: https://gist.github.com/mbostock/4573883/
+  legend
     .append("g")
-    .attr("transform", `translate(600, ${h - 20})`)
-    .attr("id", "legend");
-  legend.append("g").call(lAxis); //TODO: Repair axis to show decimal
+    .attr("transform", "translate(0, -30)")
+    .selectAll("rect")
+    .data(
+      treshold.range().map((color) => {
+        let d = treshold.invertExtent(color);
+        if (d[0] == null) d[0] = lScale.domain()[0];
+        if (d[1] == null) d[1] = lScale.domain()[1];
+        return d;
+      })
+    )
+    .enter()
+    .append("rect")
+    .attr("height", 30)
+    .attr("x", (d) => lScale(d[0]))
+    .attr("width", (d) => {
+      if (d[0] < 2.6 || d[1] > 12.8) {
+        return lScale(null);
+      }
+      return lScale(d[1]) - lScale(d[0]);
+    })
+    .attr("fill", (d) => treshold(d[0]));
 
   const dataMap = svg.append("g").attr("transform", `translate(0, -70)`);
 
@@ -115,7 +181,7 @@ const render = (data) => {
     .attr("height", 40)
     .attr("fill", "navy");
 
-  console.log(data, lMin, lMax);
+  console.log(data);
 };
 
 fetch(url)
