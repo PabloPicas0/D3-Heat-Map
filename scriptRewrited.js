946c9d387aea7380cca3d16c948a88016b0c95be
PabloPicas0 <pawel.fe@gmail.com>
2023-02-16 16:05:18 +0100

    final project

--- scriptRewrited.js
+++ scriptRewrited.js
@@ -7,7 +7,10 @@ const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 const w = 1600;
 const h = 600;
-const padding = 60;
+const margin = { top: 20, bottom: 80, left: 80, right: 40 };
+
+const innerWidth = w - margin.left - margin.right;
+const innerHeight = h - margin.bottom - margin.top;
 
 const colors = [
   "#a50026",
@@ -21,42 +24,38 @@ const colors = [
   "#74add1",
   "#4575b4",
   "#313695",
-].reverse();
+];
 
 const svg = d3.select("section").append("svg").attr("width", w).attr("height", h);
-
-const legend = svg
-  .append("g")
-  .attr("transform", `translate(600, ${h - 20})`)
-  .attr("id", "legend");
+const container = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Method for converting month number to string
 const convertMonths = (monthNumber) => {
   switch (monthNumber) {
     case 0:
-      return "December";
-    case 1:
       return "January";
-    case 2:
+    case 1:
       return "February";
-    case 3:
+    case 2:
       return "March";
-    case 4:
+    case 3:
       return "April";
-    case 5:
+    case 4:
       return "May";
-    case 6:
+    case 5:
       return "June";
-    case 7:
+    case 6:
       return "July";
-    case 8:
+    case 7:
       return "August";
-    case 9:
+    case 8:
       return "September";
-    case 10:
+    case 9:
       return "October";
-    case 11:
+    case 10:
       return "November";
+    case 11:
+      return "December";
     default:
       return "";
   }
@@ -74,7 +73,7 @@ const render = (data) => {
     element.month--;
   });
   //Base temperature
-  const baseTemp = data.baseTemperature;
+  const { baseTemperature } = data;
   const belowAvg = data.monthlyVariance.filter((elem) => elem.variance < 0);
   const aboveAvg = data.monthlyVariance.filter((elem) => elem.variance > 0);
 
@@ -87,86 +86,97 @@ const render = (data) => {
   const yMax = d3.max(data.monthlyVariance, (data) => data.month);
 
   ////Lowest and highest temperatures for legendScale
-  const lMin = d3.min(belowAvg, (data) => baseTemp + data.variance);
-  const lMax = d3.max(aboveAvg, (data) => baseTemp + data.variance);
+  const lMin = d3.min(belowAvg, (data) => baseTemperature + data.variance);
+  const lMax = d3.max(aboveAvg, (data) => baseTemperature + data.variance);
 
-  //x, y and legend Scale
-  const xScale = d3
-    .scaleLinear()
-    .domain([xMin, xMax])
-    .range([padding, w - padding]);
+  //Dimensions for cells
+  const cellWidth = (innerWidth * 12) / data.monthlyVariance.length;
+  const cellHeight = innerHeight / 12;
 
+  //x Scale
+  const xScale = d3.scaleLinear().domain([xMin, xMax]).range([margin.left, innerWidth]);
+
+  //y Scale
   const yScale = d3
     .scaleLinear()
-    .domain([yMax + 0.5, yMin - 0.5]) //We are adding some space to top and bottom of yAxis by adding this 0,5
-    .range([h - padding, padding]);
+    .domain([yMin - 0.5, yMax + 0.5]) //We are adding some space to top and bottom of yAxis by adding this 0,5
+    .range([0, innerHeight]);
 
+  //Legend Scale
   const lScale = d3
     .scaleLinear()
     .domain([lMin, lMax])
-    .range([padding, w / 4]);
+    .range([margin.right, innerWidth / 4]);
 
-  //Threshold for legend ticks
-  const treshold = d3
-    .scaleThreshold()
-    //Source for domain: FCC Project -> DevTools -> Sources -> script.js
-    .domain(
-      ((min, max, numColors) => {
-        const array = [];
-        const range = (max - min) / numColors;
-        const base = min;
-        for (let i = 1; i < numColors; i++) {
-          array.push(base + i * range);
-        }
-        return array;
-      })(lMin, lMax, colors.length)
-    )
-    .range(colors);
+  //We need to get range of values from maximum and minumum temperatures
+  const ranges = [];
+  const range = lMax - lMin;
+  const colorRanges = range / colors.length;
+  for (let i = 1; i < colors.length; i++) {
+    ranges.push(lMin + i * colorRanges);
+  }
 
-  //x, y and legend Axis
+  //Legend Scale treshold
+  const legendTreshold = d3.scaleThreshold().domain(ranges).range(colors.reverse());
+
+  //x Axis
   const xAxis = d3
     .axisBottom(xScale)
     .tickFormat((x) => Math.floor(x))
     .ticks(20);
-  //Doing axis like this was pure experiment but it works so its okay
-  const yAxis = d3.axisLeft(yScale).tickFormat((x) => convertMonths(x));
+
+  //y Axis
+  const yAxis = d3.axisLeft(yScale).tickFormat(convertMonths);
+
+  //Legend Axis
   const lAxis = d3
     .axisBottom(lScale)
-    .tickValues(treshold.domain())
+    .tickValues(legendTreshold.domain())
     .tickFormat(d3.format(".1f")) //creates axis for legend, change their ticks values and format them to fixed precision;
     .tickSize(-30);
 
-  const xAxisG = svg
+  //G element for x Axis
+  const xAxisG = container
     .append("g")
-    .attr("transform", `translate(0, ${h - (padding + 50)})`)
+    .attr("transform", `translate(0, ${innerHeight})`)
     .attr("id", "x-axis")
     .call(xAxis)
     .call((g) => g.select(".domain").remove());
 
-  const yAxisG = svg
+  //G element for y Axis
+  const yAxisG = container
     .append("g")
-    .attr("transform", `translate(${padding}, -50)`)
+    .attr("transform", `translate(${margin.left}, 0)`)
     .attr("id", "y-axis")
     .call(yAxis)
     .call((g) => g.select(".domain").remove());
 
-  const legendAxis = legend.append("g").call(lAxis);
+  //G element for legend
+  const legend = svg
+    .append("g")
+    .attr("id", "legend")
+    .attr("transform", `translate(${innerWidth / 2 - 60}, ${h - 10})`);
 
-  legendAxis.select(".domain").remove();
+  //Legend Axis
+  legend
+    .append("g")
+    .call(lAxis)
+    .call((g) => g.select(".domain").remove());
 
   //Source: https://gist.github.com/mbostock/4573883/
+  const dataset = legendTreshold.range().map((color) => {
+    let d = legendTreshold.invertExtent(color);
+    if (d[0] == null) d[0] = lScale.domain()[0];
+    if (d[1] == null) d[1] = lScale.domain()[1];
+    return d;
+  });
+
+  //Legend color ranges
   legend
     .append("g")
     .attr("transform", "translate(0, -30)")
     .selectAll("rect")
-    .data(
-      treshold.range().map((color) => {
-        let d = treshold.invertExtent(color);
-        if (d[0] == null) d[0] = lScale.domain()[0];
-        if (d[1] == null) d[1] = lScale.domain()[1];
-        return d;
-      })
-    )
+    .data(dataset)
     .enter()
     .append("rect")
     .attr("height", 30)
@@ -177,22 +187,18 @@ const render = (data) => {
       }
       return lScale(d[1]) - lScale(d[0]);
     })
-    .attr("fill", (d) => treshold(d[0]));
+    .attr("fill", (d) => legendTreshold(d[0]));
 
-  const dataMap = svg.append("g").attr("transform", `translate(0, -70)`);
-
-  //Tooltip methods
+  //Method to show data on tooltip
   const onMouseOver = (event) => {
     const tooltip = d3.select("#tooltip");
 
     //Data for tooltip
-    const temp = baseTemp;
-    const month = event.target.__data__.month;
-    const year = event.target.__data__.year;
-    const variance = event.target.__data__.variance;
+    const temp = baseTemperature;
+    const { month, year, variance } = event.target.__data__;
 
     tooltip
-      .style("top", `${event.clientY - 10}px`)
+      .style("top", `${event.clientY - 30}px`)
       .style("left", `${event.clientX - 10}px`)
       .style("opacity", "0.9")
       .attr("data-year", `${year}`)
@@ -204,30 +210,31 @@ const render = (data) => {
       );
   };
 
+  //Method for close tooltip
   const onMouseOut = () => {
-    const tooltip = d3.select("#tooltip")
+    const tooltip = d3.select("#tooltip");
 
-    tooltip.style("opacity", "0")
-  }
+    tooltip.style("opacity", "0");
+  };
 
-  dataMap
+  container
     .selectAll("rect")
     .data(data.monthlyVariance)
     .enter()
     .append("rect")
     .attr("x", (d) => xScale(d.year))
-    .attr("y", (d) => yScale(d.month))
-    .on("mouseover", onMouseOver)//TODO: When height is equal to yAxus tooltip need to be above hovered cell  
+    .attr("y", (d) => yScale(d.month) - cellHeight / 2)
+    .on("mouseover", onMouseOver)
     .on("mouseout", onMouseOut)
     .attr("data-month", (d) => d.month)
     .attr("data-year", (d) => d.year)
-    .attr("data-temp", (d) => handleTempVariance(baseTemp, d.variance))
+    .attr("data-temp", (d) => handleTempVariance(baseTemperature, d.variance))
     .attr("class", "cell")
-    .attr("width", 5)
-    .attr("height", 40)//TODO: height equal yScale
-    .attr("fill", "navy");
-
-  console.log(data, data.monthlyVariance[0].variance.toFixed(1));
+    .attr("width", cellWidth)
+    .attr("height", cellHeight)
+    .attr("fill", (d) => {
+      return legendTreshold(baseTemperature + d.variance);
+    });
 };
 
 fetch(url)
