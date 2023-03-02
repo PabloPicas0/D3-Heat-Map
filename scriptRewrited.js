27f2ceab23cc0a5de123e33adebfd7894ed4ab77
PabloPicas0 <pawel.fe@gmail.com>
2023-03-02 21:03:02 +0100

    Rewrited convertMonth function

--- scriptRewrited.js
+++ scriptRewrited.js
@@ -31,34 +31,25 @@ const container = svg.append("g").attr("transform", `translate(${margin.left}, $
 
 //Method for converting month number to string
 const convertMonths = (monthNumber) => {
-  switch (monthNumber) {
-    case 0:
-      return "January";
-    case 1:
-      return "February";
-    case 2:
-      return "March";
-    case 3:
-      return "April";
-    case 4:
-      return "May";
-    case 5:
-      return "June";
-    case 6:
-      return "July";
-    case 7:
-      return "August";
-    case 8:
-      return "September";
-    case 9:
-      return "October";
-    case 10:
-      return "November";
-    case 11:
-      return "December";
-    default:
-      return "";
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  if (monthNumber > 11) {
+    return months[0];
   }
+  return months[monthNumber];
 };
 
 const handleTempVariance = (baseTemp, variance) => {
